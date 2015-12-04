f80d57ddbe84fb13e65988ee22c87a43241e78c8
Andrew Wentz <awentz4@gmail.com>
2015-12-04 16:42:44 -0600

    Updated imports file, changed char to textfields in models

--- imports.py
+++ imports.py
@@ -1,23 +1,29 @@
 #YOU NEED THESE TO IMPORT DATA
-​
+
 import os, sys, string, csv, datetime, time, django
-​
+
 # This line imports your settings. You need to change fooproject to the name of your project
-​
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fooproject.settings")
-​
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zoo.settings")
+
 from django.conf import settings
-​
+
 #You need to change the next line to your app.models and import the name of the models in there.
-​
-from appname.models import ModelName, AnotherModel, ThirdModel
-​
+
+from zooguide.models import Animals, Exhibits, Tips
+
 from django.template.defaultfilters import slugify, urlize
-​
+
 django.setup()
-​
+
 reader = csv.reader(open("salaries1314.csv", "rU"), dialect=csv.excel)
 reader.next()
 for row in reader:
     textexample = row[0]
     integerexample = int(row[1])
+    floatexample = float(row[2])
+    dateparseexample = time.strptime(row[3], "%m/%d/%Y %H:%M")
+    dateexample = datetime.datetime(dateparseexample.tm_year, dateparseexample.tm_mon, dateparseexample.tm_mday, dateparseexample.tm_hour, dateparseexample.tm_min)
+    modnam, modnamcreated = ModelName.objects.get_or_create(name=row[4], attribute=row[5])
+    anothermod, anothermodcreated = AnotherModel.objects.get_or_create(model=modnam, other_attribute=row[7])
+    print anothermod
--- zooguide/models.py
+++ zooguide/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 class Animals(models.Model):
-     animal_name = models.CharField(max_length=35)
-     animal_text = models.CharField(max_length=250)
-
+    animal_name = models.CharField(max_length=35)
+    animal_text = models.TextField()
+    
 class Exhibits(models.Model):
-     exhibit_name = models.CharField(max_length=35)
-     exhibit_text = models.CharField(max_length=250)
+    exhibit_name = models.CharField(max_length=35)
+    exhibit_text = models.TextField()
 
 class Tips(models.Model):
-     tip_title = models.CharField(max_length=35)
-     tip_text = models.CharField(max_length=250)
+    tip_title = models.CharField(max_length=35)
+    tip_text = models.TextField()
+    exhibits = models.ManyToManyField(Exhibits)
+    tip_date = DateTimeField(auto_now)
